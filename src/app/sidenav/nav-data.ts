882d963f147141e1114d1e8aeb684a88f2c28177
Jay Mars <jmars2030_nospam@gmail.com>
2024-05-11 07:46:50 -0400

    part 1 commit

---
+++ src/app/sidenav/nav-data.ts
@@ -0,0 +1,38 @@
+export const navbarData = [
+    {
+        routeLink: 'dashboard',
+        icon: 'fal fa-home',
+        label: 'Dashboard'
+    },
+    {
+        routeLink: 'products',
+        icon: 'fal fa-box-open',
+        label: 'Products'
+    },
+    {
+        routeLink: 'statistics',
+        icon: 'fal fa-chart-bar',
+        label: 'Satistics'
+    },
+    {
+        routeLink: 'coupens',
+        icon: 'fal fa-tag',
+        label: 'Coupens'
+    },
+    {
+        routeLink: 'pages',
+        icon: 'fal fa-file',
+        label: 'Pages'
+    },
+    {
+        routeLink: 'media',
+        icon: 'fal fa-camera',
+        label: 'Media'
+    },
+    {
+        routeLink: 'settings',
+        icon: 'fal fa-cog',
+        label: 'Settings'
+    }
+
+];
